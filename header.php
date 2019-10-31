f1fabcddf08223599e04e1c8887a762c9b9a912a
kaisardesa27 <57204909+kaisardesa27@users.noreply.github.com>
2019-10-31 09:51:13 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Gopay Sender</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+          <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+</head>
+    <style type="text/css">
+        body {
+            font-family: 'Quicksand', sans-serif;
+            margin-bottom: 50px;
+        }
+    </style>
+</head>
+<body>
+    <br>
+    <div class="container">
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading"><i class="fa fa-paper-plane-o"></i> GOPAY SENDER</div>
+      <div class="panel-body">
